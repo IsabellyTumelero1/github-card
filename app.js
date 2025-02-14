699e9d03e3e49fc6951198e1ffb23182129770f1
IsabellyTumelero1 <isabellytumelero467@gmail.com>
2025-02-14 15:15:21 -0300

    finalizando o card

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-function fnCarregarDados() {
-    const url = "https://api.github.com/users/IsabellyTumelero1"
+function fnCarregarDados(usuario) {
+    const url = "https://api.github.com/users/" + usuario
+    console.log(url)
 
     fetch(url)
         .then(resposta => {
             return resposta.json()
         })
         .then(dados => {
-            fnCarregarDados(dados.avatar_url, dados.nome, dados.bio, dados.email, dados.twitter_username, dados.html_url, dados.instagram_username)
+            fnMostrarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.twitter_username, dados.html_url)
         })
 
 }
@@ -16,13 +17,34 @@ function fnMostrarDados(foto, nome, resumo, email, twitter, github, instagram) {
     document.getElementById("nome").innerHTML = nome
     document.getElementById("resumo").innerHTML = resumo + "<br><strong id='email'></strong>"
     document.getElementById("email").innerHTML = email
-    document.getElementById("twitter").href = twitter
+    document.getElementById("twitter").href = "https://x.com/" + twitter
     document.getElementById("github").href = github
-    document.getElementById("instagram").href = instagram
+    document.getElementById("instagram").href = "https://instagram.com" + instagram
+
+    if(twitter == null){
+        document.getElementById("twitter").style.display = "none"
+    }else{
+        document.getElementById("twitter").style.display = "inline"
+    }
+
+    if(instagram == null){
+        document.getElementById("instagram").style.display = "none"
+    }else{
+        document.getElementById("instagram").style.display = "inline"
+    }
 }
 
 let botao = document.getElementById("botao-carregar-dados")
 botao.addEventListener("click", function(){
-    // fnMostrarDados("fundo.jpg", "Isabelly Tumelero", "Querida", "isabellymartins301@gmail.com", "https://x.com", "https://github.com", "https://instagram.com")  --> teste
-    fnCarregarDados()
+    fnCarregarDados(document.getElementById("usuario").value)
+    document.getElementById("espaco-cartao").style.display = "block"
+    document.getElementById("espaco-botao").style.display = "none"
+
+    
 })
+
+let btnVoltar = document.getElementById("botao-voltar")
+    btnVoltar.addEventListener("click", function(){
+        document.getElementById("espaco-cartao").style.display = "none"
+    document.getElementById("espaco-botao").style.display = "block"
+    })
